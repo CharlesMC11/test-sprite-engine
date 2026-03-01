26915711c8aaef375e83f664ca04fe8b57b2c318
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-02-28 22:22:55 -0800

    Save header

--- include/sprite.h
+++ include/sprite.h
@@ -1,36 +1,47 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
-#include <stdint.h>
+#ifdef __cplusplus
+#include <cstdint>
 
-#define HEIGHT 32
+namespace sc {
+    extern "C" {
+#else
+#include <stdint.h>
+#endif
+
 #define WIDTH 32
+#define HEIGHT 32
 #define MAX_PALETTE_SIZE 16
 
-// BGRA
-typedef uint16_t Color;
+    // Color Bit Distribution
+    typedef enum ColorMode : uint8_t {
+        DEFAULT = 1, // 5-6-5
+        WARM = 2, // 6-5-5
+        FOREST = 3, // 3-10-3
+        SEA = 5, // 2-2-12
+    } ColorMode;
 
-typedef enum : uint8_t {
-  // BGRA
-  CHAR = 1,   // 5/6/5/0
-  FOREST = 3, // 3/10/3/0
-  SEA = 5,    // 2/2/12
-} Mode;
+    // BRGA
+    typedef uint16_t Color;
 
-typedef struct {
-  uint8_t index : 3;
-  uint8_t glow : 2;
-  uint8_t reserved : 3;
-} Pixel;
+    typedef struct {
+        uint8_t index : 4;
+        uint8_t reserved : 4;
+    } Pixel;
 
-typedef struct {
-  char magic[8];
-  Mode mode;
-  uint8_t anchor_x;
-  uint8_t anchor_y;
-  uint8_t reserved[21];
-  Color palette[MAX_PALETTE_SIZE];
-  Pixel pixels[HEIGHT * WIDTH];
-} Sprite;
+    typedef struct alignas(32) Sprite {
+        char magic[8];
+        ColorMode mode;
+        uint8_t anchor_x;
+        uint8_t anchor_y;
+        Color palette[MAX_PALETTE_SIZE];
+        Pixel pixels[WIDTH * HEIGHT];
+    } Sprite;
 
-#endif // PIXEL_H
+#ifdef __cplusplus
+    } // extern "C"
+} // namespace sc
+#endif
+
+#endif // SPRITE_H
