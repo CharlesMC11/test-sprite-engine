250f12c3fac3a6ae33771ec909ca82ef67dc2793
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-03-07 13:08:29 -0800

    Use concept and attributes

--- include/memory_map.hpp
+++ include/memory_map.hpp
@@ -6,59 +6,86 @@
 #include <unistd.h>
 
 #include <string_view>
+#include <type_traits>
 
 namespace sc {
 
     template<typename T>
-    class memory_map {
+    concept mappable = alignof(T) == 16 && std::is_trivially_copyable_v<T> &&
+            std::is_standard_layout_v<T>;
+
+    template<mappable T>
+    class memory_map final {
     public:
-        explicit memory_map(const char path[]);
-        ~memory_map();
+        [[nodiscard]] explicit memory_map(const char path[]) noexcept;
+        ~memory_map() noexcept;
 
         memory_map(const memory_map&) = delete;
         memory_map& operator=(const memory_map&) = delete;
 
-        const T* operator->() const noexcept { return buffer_; }
-        const T& operator*() const noexcept { return *buffer_; }
+        [[nodiscard]] constexpr const T* operator->() const noexcept;
+        [[nodiscard]] constexpr const T& operator*() const noexcept;
+        [[nodiscard]] explicit constexpr operator bool() const noexcept;
 
-        explicit operator bool() const noexcept;
-
-        const T* data() const noexcept { return buffer_; }
+        [[nodiscard]] constexpr const T* data() const noexcept;
 
     private:
         const T* buffer_ = nullptr;
     };
 
-    template<typename T>
-    memory_map<T>::memory_map(const char path[])
+    template<mappable T>
+    memory_map<T>::memory_map(const char path[]) noexcept
     {
         const int fd{open(path, O_RDONLY)};
-        if (fd < 0)
+        if (fd < 0) [[unlikely]]
             return;
 
         struct stat st;
-        if (fstat(fd, &st) < 0 || st.st_size != sizeof(T)) {
+        if (fstat(fd, &st) < 0 || st.st_size != sizeof(T)) [[unlikely]] {
             close(fd);
             return;
         }
 
-        buffer_ = static_cast<T*>(
-                mmap(nullptr, sizeof(T), PROT_READ, MAP_SHARED, fd, 0));
+        const void* result =
+                mmap(nullptr, sizeof(T), PROT_READ, MAP_SHARED, fd, 0);
+        if (result == MAP_FAILED) [[unlikely]] {
+            close(fd);
+            return;
+        }
 
+        buffer_ = static_cast<const T*>(result);
         close(fd);
     }
 
-    template<typename T>
-    memory_map<T>::~memory_map()
+    template<mappable T>
+    memory_map<T>::~memory_map() noexcept
     {
-        if (*this)
+        if (buffer_)
             munmap(const_cast<T*>(buffer_), sizeof(T));
     }
 
-    template<typename T>
-    memory_map<T>::operator bool() const noexcept
+    template<mappable T>
+    [[nodiscard]] constexpr const T* memory_map<T>::operator->() const noexcept
     {
-        return buffer_ != nullptr && buffer_ != MAP_FAILED;
+        return buffer_;
+    }
+
+    template<mappable T>
+    [[nodiscard]] constexpr const T& memory_map<T>::operator*() const noexcept
+    {
+        return *buffer_;
+    }
+
+    template<mappable T>
+    [[nodiscard]] constexpr memory_map<T>::operator bool() const noexcept
+    {
+        return buffer_ != nullptr;
+    }
+
+    template<mappable T>
+    [[nodiscard]] constexpr const T* memory_map<T>::data() const noexcept
+    {
+        return buffer_;
     }
 
 } // namespace sc
