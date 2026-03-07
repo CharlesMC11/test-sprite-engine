243af51edb1b2b0eaf9e1c90fc5c486c560f06e9
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-03-06 22:14:38 -0800

    Switch to C++

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include "memory_map.hpp"
+#include "sprite.h"
+
+extern void render(const sc::sprite* sprite);
+
+void debug_sprite(const sc::memory_map<sc::sprite>& sprite);
+
+int main(const int argc, const char* argv[])
+{
+    if (argc < 2)
+        return 1;
+
+    const sc::memory_map<sc::sprite> loader{argv[1]};
+    if (!loader)
+        return 1;
+
+    debug_sprite(loader);
+
+    // render(loader.data());
+}
+
+void debug_sprite(const sc::memory_map<sc::sprite>& sprite)
+{
+    printf("Encoding: %d\n",
+            sprite->encoding); //
+    for (uint_fast8_t y = 0; y < HEIGHT; ++y) {
+        for (uint_fast8_t x = 0; x < WIDTH; ++x) {
+            const uint_fast8_t i = sprite->pixels[y * WIDTH + x].index;
+
+            printf("%c", i > 0 ? '#' : ' ');
+        }
+        puts("");
+    }
+}
