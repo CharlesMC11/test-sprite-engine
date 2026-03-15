97e8d21672eec4a610bb6858cfdf1a29d4f5843f
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-03-15 00:07:58 -0700

    Make metal-compatible

--- include/bbox.hh
+++ include/bbox.hh
@@ -5,10 +5,22 @@ namespace sc::geometry {
     template<typename T>
     struct bbox final {
 
-        constexpr T width() const noexcept { return right - left; }
-        constexpr T height() const noexcept { return bottom - top; }
+        [[nodiscard]] constexpr T width() const;
+        [[nodiscard]] constexpr T height() const;
 
         T left, top, right, bottom;
     };
+
+    template<typename T>
+    [[nodiscard]] constexpr T bbox<T>::width() const
+    {
+        return right - left;
+    }
+
+    template<typename T>
+    [[nodiscard]] constexpr T bbox<T>::height() const
+    {
+        return bottom - top;
+    }
 
 } // namespace sc::geometry
