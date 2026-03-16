e73af33b13f9e0be347ac44a33746d60d7598f19
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-03-15 23:12:47 -0700

    Minor rename

--- include/scene_registry.hh
+++ include/scene_registry.hh
@@ -1,5 +1,5 @@
 /**
- * @file scene_population.hh
+ * @file scene_registry.hh
  * @brief
  */
 #pragma once
@@ -60,7 +60,7 @@ namespace sc {
         constexpr void sort_draw() noexcept;
 
         /// TODO: Make custom allocator?
-        std::vector<float> x, y, z, dx, dy, dz, next_x, next_y, next_z;
+        std::vector<float> x, y, z, vx, vy, vz, next_x, next_y, next_z;
         std::vector<sprites::atlas_index> indices;
         std::vector<core::index_t> draw_order;
 
@@ -88,9 +88,9 @@ namespace sc {
             x.reserve(aligned);
             y.reserve(aligned);
             z.reserve(aligned);
-            dx.reserve(aligned);
-            dy.reserve(aligned);
-            dz.reserve(aligned);
+            vx.reserve(aligned);
+            vy.reserve(aligned);
+            vz.reserve(aligned);
             next_x.reserve(aligned);
             next_y.reserve(aligned);
             next_z.reserve(aligned);
@@ -109,9 +109,9 @@ namespace sc {
         x.push_back(start_x);
         y.push_back(start_y);
         z.push_back(start_z);
-        dx.push_back(0.0f);
-        dy.push_back(0.0f);
-        dz.push_back(0.0f);
+        vx.push_back(0.0f);
+        vy.push_back(0.0f);
+        vz.push_back(0.0f);
         next_x.push_back(start_x);
         next_y.push_back(start_y);
         next_z.push_back(start_z);
@@ -133,13 +133,13 @@ namespace sc {
                 const float32x4_t v_y{vld1q_f32(&y[i])};
                 const float32x4_t v_z{vld1q_f32(&z[i])};
 
-                const float32x4_t v_dx{vld1q_f32(&dx[i])};
-                const float32x4_t v_dy{vld1q_f32(&dy[i])};
-                const float32x4_t v_dz{vld1q_f32(&dz[i])};
+                const float32x4_t v_vx{vld1q_f32(&vx[i])};
+                const float32x4_t v_vy{vld1q_f32(&vy[i])};
+                const float32x4_t v_vz{vld1q_f32(&vz[i])};
 
-                const float32x4_t v_next_x{vfmaq_f32(v_x, v_dx, v_dt)};
-                const float32x4_t v_next_y{vfmaq_f32(v_y, v_dy, v_dt)};
-                const float32x4_t v_next_z{vfmaq_f32(v_z, v_dz, v_dt)};
+                const float32x4_t v_next_x{vfmaq_f32(v_x, v_vx, v_dt)};
+                const float32x4_t v_next_y{vfmaq_f32(v_y, v_vy, v_dt)};
+                const float32x4_t v_next_z{vfmaq_f32(v_z, v_vz, v_dt)};
 
                 vst1q_f32(&next_x[i], v_next_x);
                 vst1q_f32(&next_y[i], v_next_y);
@@ -148,9 +148,9 @@ namespace sc {
         }
 
         for (core::index_t i{vectorized_lim}; i < n; ++i) {
-            next_x[i] = x[i] + dx[i] * dt;
-            next_y[i] = y[i] + dy[i] * dt;
-            next_z[i] = z[i] + dz[i] * dt;
+            next_x[i] = x[i] + vx[i] * dt;
+            next_y[i] = y[i] + vy[i] * dt;
+            next_z[i] = z[i] + vz[i] * dt;
         }
     }
 
