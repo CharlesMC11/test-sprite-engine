accc928457a4d88eb5208aff0f4b2104bedc0d6a
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-03-27 01:12:16 -0700

    Add offsets

--- include/render_bridge.hh
+++ include/render_bridge.hh
@@ -56,6 +56,8 @@ namespace sc {
         NS::SharedPtr<MTL::ComputePipelineState> clear_pso_{nullptr};
         NS::SharedPtr<MTL::ComputePipelineState> sprite_pso_{nullptr};
         NS::SharedPtr<MTL::Buffer> sprite_buffer_{nullptr};
+        NS::UInteger palette_offset_{0u};
+        NS::UInteger sprites_offset_{0u};
 
         MTL::CommandBuffer* command_buffer_{nullptr};
         MTL::ComputeCommandEncoder* encoder_{nullptr};
