b8bc0fbf097954327ec56eaa1f94328c7bd7bff7
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-03-29 11:33:40 -0700

    Move to C++ funcs

--- src/entity_registry.cc
+++ src/entity_registry.cc
@@ -1,15 +1,17 @@
 #include "entity_registry.hh"
 
 #include <cstddef>
-#include <cstdio>
+#include <format>
+#include <iostream>
 
 namespace sc {
 
-    void entity_registry::print() const noexcept
+    void entity_registry::print() const
     {
         for (std::size_t i{0u}; i < count(); ++i) {
-            std::printf("Entity %zu (Sprite Index: %u)\n\tpos (%7.2f, %7.2f, "
-                        "%7.2f)\n\tvec <%7.2f, %7.2f, %7.2f>\n\n",
+            std::cout << std::format(
+                    "Entity {} (Sprite Index: {})\n\tpos ({:7.2f}, {:7.2f}, "
+                    "{:7.2f})\n\tvec <{:7.2f}, {:7.2f}, {:7.2f}>\n\n",
                     i, static_cast<core::index_t>(indices[i]), pos_x_ptr()[i],
                     pos_y_ptr()[i], pos_z_ptr()[i], vec_x_ptr()[i],
                     vec_y_ptr()[i], vec_z_ptr()[i]);
