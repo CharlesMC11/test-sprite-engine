645fedbd39cb43c4d485210326bf03d5e091171e
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-03-30 03:22:07 -0700

    Create bitwise enum template

--- src/core/core.hh
+++ src/core/core.hh
@@ -40,12 +40,25 @@ namespace sc {
         concept mappable = alignof(T) % kNeonAlignment == 0 &&
                 std::is_standard_layout_v<T> && !std::is_polymorphic_v<T>;
 
+        template<typename T>
+        constexpr bool enable_bitwise_ops_v = false;
+
+        template<typename T>
+        concept bitwise_enum =
+                std::is_enum_v<T> && sc::core::enable_bitwise_ops_v<T>;
+
+        template<bitwise_enum T>
+        [[nodiscard]] constexpr bool any(const T val) noexcept
+        {
+            return static_cast<std::underlying_type_t<T>>(val) != 0;
+        }
+
         using float_limits = std::numeric_limits<float>;
 
         static constexpr float kEpsilon{float_limits::epsilon()};
         static constexpr float kInfinity{float_limits::infinity()};
 
-#endif
+#endif // __METAL_VERSION__
 
     } // namespace core
 
@@ -70,5 +83,59 @@ namespace sc {
     } // namespace display
 
 } // namespace sc
+
+#ifndef __METAL_VERSION__
+
+template<sc::core::bitwise_enum T>
+[[nodiscard]] constexpr T operator|(const T lhs, const T rhs) noexcept
+{
+    using U = std::underlying_type_t<T>;
+    return static_cast<T>(static_cast<U>(lhs) | static_cast<U>(rhs));
+}
+
+template<sc::core::bitwise_enum T>
+[[nodiscard]] constexpr T operator&(const T lhs, const T rhs) noexcept
+{
+    using U = std::underlying_type_t<T>;
+    return static_cast<T>(static_cast<U>(lhs) & static_cast<U>(rhs));
+}
+
+template<sc::core::bitwise_enum T>
+[[nodiscard]] constexpr T operator^(const T lhs, const T rhs) noexcept
+{
+    using U = std::underlying_type_t<T>;
+    return static_cast<T>(static_cast<U>(lhs) ^ static_cast<U>(rhs));
+}
+
+template<sc::core::bitwise_enum T>
+[[nodiscard]] constexpr T operator~(const T val) noexcept
+{
+    using U = std::underlying_type_t<T>;
+    return static_cast<T>(~static_cast<U>(val));
+}
+
+template<sc::core::bitwise_enum T>
+constexpr T& operator|=(T& lhs, const T rhs) noexcept
+{
+    return lhs = lhs | rhs;
+}
+
+template<sc::core::bitwise_enum T>
+constexpr T& operator&=(T& lhs, const T rhs) noexcept
+{
+    return lhs = lhs & rhs;
+}
+
+template<sc::core::bitwise_enum T>
+constexpr T& operator^=(T& lhs, const T rhs) noexcept
+{
+    return lhs = lhs ^ rhs;
+}
+
+#define SC_ENABLE_ENUM_BITWISE_OPS(EnumType)                                   \
+    template<>                                                                 \
+    constexpr bool sc::core::enable_bitwise_ops_v<EnumType> = true;
+
+#endif // __METAL_VERSION__
 
 #endif // SC_CORE_CORE_HH
--- src/core/input.hh
+++ src/core/input.hh
@@ -1,39 +1,22 @@
 #ifndef SC_CORE_INPUT_HH
 #define SC_CORE_INPUT_HH
 
+#include <cstdint>
+
 #include "core/core.hh"
 
 namespace sc::input {
 
-    enum class mask : core::input_mask {
-        UP = 0x01,
-        DOWN = 0x02,
-        LEFT = 0x04,
-        RIGHT = 0x08,
+    enum class mask : std::uint8_t {
+        NONE = 0u,
+        UP = 1u,
+        DOWN = 1u << 1u,
+        LEFT = 1u << 2u,
+        RIGHT = 1u << 3u,
     };
 
-    constexpr core::input_mask operator&(const core::input_mask a, mask b)
-    {
-        return a & static_cast<core::input_mask>(b);
-    }
-
-    constexpr core::input_mask& operator&=(core::input_mask& a, mask b)
-    {
-        a &= static_cast<core::input_mask>(b);
-        return a;
-    }
-
-    constexpr core::input_mask& operator|=(core::input_mask& a, mask b)
-    {
-        a |= static_cast<core::input_mask>(b);
-        return a;
-    }
-
-    constexpr core::input_mask operator~(const mask& m)
-    {
-        return ~static_cast<core::input_mask>(m);
-    }
-
 } // namespace sc::input
+
+SC_ENABLE_ENUM_BITWISE_OPS(sc::input::mask)
 
 #endif // SC_CORE_INPUT_HH
